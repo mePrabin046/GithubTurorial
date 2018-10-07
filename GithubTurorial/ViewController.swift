c33cc4697fdee29f6b9cbe54e2f74f99f986d302
Prabin <rprabin046@gmail.com>
2018-10-07 14:13:32 +0545

    Initial Commit

---
+++ GithubTurorial/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GithubTurorial
+//
+//  Created by Prabin Pariyar on 10/7/18.
+//  Copyright © 2018 Prabin Pariyar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
